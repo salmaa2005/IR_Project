6c4b4e94b522dd940dc79cf1f438ce43530e03cb
salomy <salmaamostafa2005@gmail.com>
2024-12-12 00:42:53 +0200

    add IndexBuilder v2.3

--- sourceCode/TokenizerMapper.java
+++ sourceCode/TokenizerMapper.java
@@ -9,14 +9,11 @@ public class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
     private Text word = new Text();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        // Get the file name as the document ID
         String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
 
-        // Read the line from the file
         String line = value.toString();
         String[] tokens = line.split(" ");
 
-        // Process each token and write to context
         for (int position = 0; position < tokens.length; position++) {
             String token = tokens[position];
             context.write(new Text(token), new Text(fileName + ":" + position));
